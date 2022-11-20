257b94685d55cb3aa319206632a352cd93c377fb
iThorgrim <110770111+iThorgrim@users.noreply.github.com>
2022-11-20 14:15:58 +0100

    Update Run.cs

--- BlizzardNet/BlizzardNet.Tests/Run.cs
+++ BlizzardNet/BlizzardNet.Tests/Run.cs
@@ -10,21 +10,6 @@ namespace BlizzardNet.Tests
     public static class Run
     {
         public static async Task Main(string[] args)
-        {
-            // Arrange
-            var client = new Client("CLINET_ID", "CLIENT_SECRET", "eu", "fr_FR");
-            
-            // Act
-            Link.Locale = "fr_FR";
-            Link.Region = "eu";
-            
-            QuestAreasIndex questArea = await client.Get<QuestAreasIndex>(Link.QuestAreasIndex());
-            List<QuestArea> questDataFormArea = questArea.areas.Select(areas => client.Get<QuestArea>(Link.QuestArea(areas.id)).Result).ToList();
-
-            List<Quest> quests = questDataFormArea.SelectMany(qa => qa.quests.Select(quest => client.Get<Quest>(Link.Quest(quest.id)).Result)).ToList();
-            
-            // Assert
-            Assert.IsNotNull(quests);
-        }
+        { }
     }
 }
